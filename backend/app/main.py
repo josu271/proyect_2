9e5d8728319093e8e6834b8e725cb801a6bb7f2a
josu271 <sullacorbetta@gmail.com>
2026-04-27 10:30:54 -0500

    backend teacher 2

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ from app.modules.admin.estudiantes.estudiantes_routes import router as estudiant
 from app.modules.admin.docentes.docentes_routes import router as docentes_router
 from app.modules.admin.cursos.cursos_routes import router as cursos_router
 from app.modules.teacher.disponibilidad.disponibilidad_routes import router as disponibilidad_router
+from app.modules.teacher.cursos.cursos_routes import router as teacher_cursos_router
 
 
 app = FastAPI()
@@ -23,6 +24,7 @@ app.include_router(estudiantes_router, prefix="/admin/estudiantes", tags=["Admin
 app.include_router(docentes_router,prefix="/admin/docentes",tags=["Admin - Docentes"])
 app.include_router(cursos_router,prefix="/admin/cursos",tags=["Admin - Cursos"])
 app.include_router(disponibilidad_router, prefix="/teacher/disponibilidad", tags=["Teacher - Disponibilidad"])
+app.include_router(teacher_cursos_router, prefix="/teacher/cursos", tags=["Teacher - Cursos"])
 
 @app.get("/")
 def inicio():
